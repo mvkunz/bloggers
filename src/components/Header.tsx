e31a7437e869cd4772234942542fc724cd30ac21
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-08-02 20:04:48 -0300

    second requirement ok

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/Header.css';
 
 export default function Header() {
   return (
-    <header>
-      <h2>Bloggers</h2>
-    </header>
+    <>
+      <header>
+        <h2>Bloggers</h2>
+      </header>
+      <nav>
+      <NavLink to="/">HOME</NavLink>
+      <NavLink to="/about">ABOUT</NavLink>
+      </nav>
+    </>
   );
 }
