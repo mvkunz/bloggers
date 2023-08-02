9e6e3fa477be54689721403c7be5a07b0bb79ab8
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-08-02 19:59:08 -0300

    first requirement ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { Routes , Route } from 'react-router-dom';
+// import React from 'react';
 import './styles/App.css';
 import { About, NotFound, Posts, Users } from './pages';
 
 function App() {
-  return (<p>WIP</p>);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={ <Users /> } />
+        <Route path="/posts/:id" element={ <Posts /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/*" element={ <NotFound /> } />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
